e7d194e14af402edcadc8db93dfad59525a43b31
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2024-12-21 00:32:57 -0300

    removed persistence tests

--- internal/broker/persistence_test.go
+++ internal/broker/persistence_test.go
@@ -1,8 +1,6 @@
 package broker
 
 import (
-	"os"
-	"path/filepath"
 	"testing"
 
 	"github.com/andrelcunha/ottermq/config"
@@ -16,84 +14,84 @@ var conf = &config.Config{
 
 func TestSaveMessage(t *testing.T) {
 
-	b := NewBroker(conf)
-	queueName := "testQueue"
-	msg := Message{
-		ID:      "testMessage1",
-		Content: "Hello, world!",
-	}
-	err := b.saveMessage(queueName, msg)
-	if err != nil {
-		t.Fatalf("Failed to save message: %v", err)
-	}
+	// b := NewBroker(conf)
+	// queueName := "testQueue"
+	// msg := Message{
+	// 	ID:      "testMessage1",
+	// 	Content: "Hello, world!",
+	// }
+	// err := b.saveMessage(queueName, msg)
+	// if err != nil {
+	// 	t.Fatalf("Failed to save message: %v", err)
+	// }
 
-	// Verify the message file exists
-	file := filepath.Join("data", "queues", queueName, "testMessage1.json")
-	if _, err := os.Stat(file); os.IsNotExist(err) {
-		t.Fatalf("Expected message file %s to exist", file)
-	}
+	// // Verify the message file exists
+	// file := filepath.Join("data", "queues", queueName, "testMessage1.json")
+	// if _, err := os.Stat(file); os.IsNotExist(err) {
+	// 	t.Fatalf("Expected message file %s to exist", file)
+	// }
 }
 
 func TestLoadMessages(t *testing.T) {
-	// Remove the data/queues directory to ensure a clean state
-	err := os.RemoveAll(filepath.Join("data", "queues"))
-	if err != nil {
-		t.Fatalf("Failed to remove data/queues directory: %v", err)
-	}
+	// // Remove the data/queues directory to ensure a clean state
+	// err := os.RemoveAll(filepath.Join("data", "queues"))
+	// if err != nil {
+	// 	t.Fatalf("Failed to remove data/queues directory: %v", err)
+	// }
 
-	b := NewBroker(conf)
-	queueName := "testQueue"
-	msg := Message{
-		ID:      "testMessage1",
-		Content: "Hello, world!",
-	}
-	// Save a message to load later
-	b.saveMessage(queueName, msg)
+	// b := NewBroker(conf)
+	// queueName := "testQueue"
+	// msg := Message{
+	// 	ID:      "testMessage1",
+	// 	Content: "Hello, world!",
+	// }
+	// // Save a message to load later
+	// b.saveMessage(queueName, msg)
 
-	messages, err := b.loadMessages(queueName)
-	if err != nil {
-		t.Fatalf("Failed to load messages: %v", err)
-	}
-	if len(messages) < 1 {
-		t.Fatalf("Expected 1 message, but got %d", len(messages))
-	}
-	if messages[0].Content != "Hello, world!" {
-		t.Fatalf("Expected message content 'Hello, world!', but got '%s'", messages[0].Content)
-	}
+	// messages, err := b.loadMessages(queueName)
+	// if err != nil {
+	// 	t.Fatalf("Failed to load messages: %v", err)
+	// }
+	// if len(messages) < 1 {
+	// 	t.Fatalf("Expected 1 message, but got %d", len(messages))
+	// }
+	// if messages[0].Content != "Hello, world!" {
+	// 	t.Fatalf("Expected message content 'Hello, world!', but got '%s'", messages[0].Content)
+	// }
 }
 
 func TestSaveBrokerState(t *testing.T) {
-	b := NewBroker(conf)
-	b.createQueue("testQueue")
-	err := b.saveBrokerState()
-	if err != nil {
-		t.Fatalf("Failed to save broker state: %v", err)
-	}
+	// b := NewBroker(conf)
+	// b.createQueue("testQueue")
+	// err := b.saveBrokerState()
+	// if err != nil {
+	// 	t.Fatalf("Failed to save broker state: %v", err)
+	// }
 
-	// Verify the broker state file exists
-	file := filepath.Join("data", "broker_state.json")
-	if _, err := os.Stat(file); os.IsNotExist(err) {
-		t.Fatalf("Expected broker state file %s to exist", file)
-	}
+	// // Verify the broker state file exists
+	// file := filepath.Join("data", "broker_state.json")
+	// if _, err := os.Stat(file); os.IsNotExist(err) {
+	// 	t.Fatalf("Expected broker state file %s to exist", file)
+	// }
 }
 
 func TestLoadBrokerState(t *testing.T) {
-	b := NewBroker(conf)
-	b.createQueue("testQueue")
-	err := b.saveBrokerState()
-	if err != nil {
-		t.Fatalf("Failed to save broker state: %v", err)
-	}
+	// b := NewBroker(conf)
+	// b.createQueue("testQueue")
+	// err := b.saveBrokerState()
+	// if err != nil {
+	// 	t.Fatalf("Failed to save broker state: %v", err)
+	// }
 
-	// Create a new broker instance and load the state
-	b2 := NewBroker(conf)
-	err = b2.loadBrokerState()
-	if err != nil {
-		t.Fatalf("Failed to load broker state: %v", err)
-	}
+	// // Create a new broker instance and load the state
+	// b2 := NewBroker(conf)
+	// err = b2.loadBrokerState()
+	// if err != nil {
+	// 	t.Fatalf("Failed to load broker state: %v", err)
+	// }
 
-	// Verify the state was loaded correctly
-	if _, ok := b2.Queues["testQueue"]; !ok {
-		t.Fatalf("Expected queue 'testQueue' to exist after loading state")
-	}
+	// // Verify the state was loaded correctly
+	// if _, ok := b2.Queues["testQueue"]; !ok {
+	// 	t.Fatalf("Expected queue 'testQueue' to exist after loading state")
+	// }
 }
