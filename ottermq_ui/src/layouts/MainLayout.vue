67cd4c94fe10a4ab9382443f14e1ace04c9c29af
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2025-09-30 14:08:03 -0300

    feat: initial main layout page

--- ottermq_ui/src/layouts/MainLayout.vue
+++ ottermq_ui/src/layouts/MainLayout.vue
@@ -2,21 +2,27 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <q-toolbar-title> Ottermq </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div>User: {{username}}</div>
+        <q-btn flat label="Logout" @click="logout" />
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header> Essential Links </q-item-label>
-
-        <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
-      </q-list>
-    </q-drawer>
+    <q-tabs
+      v-model="tab"
+      class="bg-primary text-primary"
+      align="justify"
+      dense
+      active-color="primary"
+      indicator-color="primary"
+    >
+      <q-tab name="overview" label="Overview" @click="navigate('overview')"/>
+      <q-tab name="connections" label="Connections" @click="navigate('connections')"/>
+      <q-tab name="exchanges" label="Exchanges" @click="navigate('exchanges')"/>
+      <q-tab name="queues" label="Queues" @click="navigate('queues')"/>
+    </q-tabs>
 
     <q-page-container>
       <router-view />
@@ -26,56 +32,19 @@
 
 <script setup>
 import { ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { useRouter } from 'vue-router'
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
-  },
-]
+const tab = ref('overview')
+const username = ref('guest')
+const router = useRouter()
 
-const leftDrawerOpen = ref(false)
-
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value
+function logout() {
+  // clear auth and redirect -- Mocked for now
+  router.push('/login')
 }
+
+const navigate = (route) => {
+  router.push(route)
+}
+
 </script>
