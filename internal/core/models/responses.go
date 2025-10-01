8f02ceace7412ff245f6a77e3d04f740fe06de68
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2025-09-30 21:24:33 -0300

    feat: update API and WebAPI

---
+++ internal/core/models/responses.go
@@ -0,0 +1,17 @@
+package models
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+type SuccessResponse struct {
+	Message string `json:"message"`
+}
+
+type ConnectionListResponse struct {
+	Connections []ConnectionInfoDTO `json:"connections"`
+}
+
+type ExchangeListResponse struct {
+	Exchanges []ExchangeDTO `json:"exchanges"`
+}
