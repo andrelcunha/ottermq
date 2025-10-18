cf77ec97b8c919a3fba2b7cb4a47e2ffa22f0f84
Andre Luis da Cunha <andrelcunha81@gmail.com>
2025-10-18 15:24:55 -0300

    🐛 fix: load persisted queues (including messages) before exchanges

--- internal/core/broker/vhost/vhost.go
+++ internal/core/broker/vhost/vhost.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net"
 	"sync"
+	"time"
 
 	"github.com/andrelcunha/ottermq/internal/core/amqp"
 	"github.com/andrelcunha/ottermq/internal/persistdb"
@@ -98,6 +99,43 @@ func (vh *VHost) loadPersistedState() {
 	if vh.persist == nil {
 		return
 	}
+	// Load queues
+	queues, err := vh.persist.LoadAllQueues(vh.Name)
+	if err != nil {
+		panic("failed to load queues")
+	}
+	for _, queue := range queues {
+		vh.Queues[queue.Name] = NewQueue(queue.Name, vh.queueBufferSize)
+		vh.Queues[queue.Name].Props = &QueueProperties{
+			Durable:    queue.Properties.Durable,
+			AutoDelete: queue.Properties.AutoDelete,
+			Exclusive:  queue.Properties.Exclusive,
+			Arguments:  queue.Properties.Arguments,
+		}
+		// Load messages into the queue
+		for _, msgData := range queue.Messages {
+			msg := amqp.Message{
+				ID:   msgData.ID,
+				Body: msgData.Body,
+				Properties: amqp.BasicProperties{
+					ContentType:     amqp.ContentType(msgData.Properties.ContentType),
+					ContentEncoding: msgData.Properties.ContentEncoding,
+					Headers:         msgData.Properties.Headers,
+					DeliveryMode:    amqp.DeliveryMode(msgData.Properties.DeliveryMode),
+					Priority:        msgData.Properties.Priority,
+					CorrelationID:   msgData.Properties.CorrelationID,
+					ReplyTo:         msgData.Properties.ReplyTo,
+					Expiration:      msgData.Properties.Expiration,
+					MessageID:       msgData.Properties.MessageID,
+					Timestamp:       time.Unix(msgData.Properties.Timestamp, 0),
+					Type:            msgData.Properties.Type,
+					UserID:          msgData.Properties.UserID,
+					AppID:           msgData.Properties.AppID,
+				},
+			}
+			vh.Queues[queue.Name].Push(msg)
+		}
+	}
 
 	// Load exchanges
 	exchanges, err := vh.persist.LoadAllExchanges(vh.Name)
@@ -112,20 +150,8 @@ func (vh *VHost) loadPersistedState() {
 			Arguments:  exchange.Properties.Arguments,
 		}
 		vh.Exchanges[exchange.Name] = NewExchange(exchange.Name, typ, props)
-	}
-
-	// Load queues
-	queues, err := vh.persist.LoadAllQueues(vh.Name)
-	if err != nil {
-		panic("failed to load queues")
-	}
-	for _, queue := range queues {
-		vh.Queues[queue.Name] = NewQueue(queue.Name, vh.queueBufferSize)
-		vh.Queues[queue.Name].Props = &QueueProperties{
-			Durable:    queue.Properties.Durable,
-			AutoDelete: queue.Properties.AutoDelete,
-			Exclusive:  queue.Properties.Exclusive,
-			Arguments:  queue.Properties.Arguments,
+		for _, bindingData := range exchange.Bindings {
+			vh.BindQueue(exchange.Name, bindingData.QueueName, bindingData.RoutingKey)
 		}
 	}
 }
